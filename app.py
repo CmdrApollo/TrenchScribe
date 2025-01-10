950893f522a457893714e08caa2595b03b560cdc
CmdrApollo <henryj2545@gmail.com>
2025-01-10 16:33:17 -0600

    added comments and fixed a minor bug

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
+# Trench Scribe v0.2.2
+
+# imports
 from flask import Flask, render_template, request, send_file
-import os, json, time
+import os, json
+
+from typing import Any
 
 from reportlab.lib.pagesizes import letter
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, Spacer, PageBreak
@@ -7,21 +12,22 @@ from reportlab.lib.styles import getSampleStyleSheet
 
 inch = 72
 
+# json data for description/equipment descriptions
 addons = json.load(open(os.path.join('data', 'addons.json'), 'rb'))
 equipment = json.load(open(os.path.join('data', 'equipment.json'), 'rb'))
 
+# reportlab is used to actually generate the pdf files
 from reportlab.lib.styles import ParagraphStyle, getSampleStyleSheet
 from reportlab.lib import colors
 
-# Create a new stylesheet object
-custom_styles = getSampleStyleSheet()
-
-def literal(x):
+# puts a '+' in front of positive integers
+def literal(x: int) -> str:
     if x > 0:
         return "+" + str(x)
     return str(x)
 
-def split(line):
+# splits a single string into lines of ~60 chars
+def split(line: str) -> list[str]:
     max_len = 60
     final = ['']
 
@@ -32,7 +38,11 @@ def split(line):
 
     return final
 
-def cursed(obj):
+# i don't even know how to describe this
+# it extracts data from certain objects
+# i made this function so that bulleted
+# lists would work properly
+def cursed(obj: dict[Any: Any]):
     final = ""
     final += "\n".join(split(obj["description"][0]["content"]))
     try:
@@ -43,7 +53,9 @@ def cursed(obj):
         pass
     return final
 
-def cursed_weapon(obj):
+# pretty much same as cursed but made to
+# work with weapons
+def cursed_weapon(obj: dict[Any: Any]):
     final = ""
     final += "\n".join(split(obj["Description"][0]["SubContent"][0]["Content"]))
     try:
@@ -54,13 +66,15 @@ def cursed_weapon(obj):
         pass
     return final
 
+# searches the json data for a specific 'addon'/upgrade
 def get_addon(a):
     for add in addons:
         if add['id'] == a:
             return add
         
-    return None
+    return addons[0]
 
+# searches the json data for a specific piece of equipment
 def get_equipment(a):
     for add in equipment:
         if add['id'] == a:
@@ -69,43 +83,50 @@ def get_equipment(a):
     return equipment[0]
 
 def generate_pdf_with_table(data, ignore_tough, corner_rounding, page_splitting, color):
-    # Modify existing styles or add new ones
-    try:
-        custom_styles.add(ParagraphStyle(
-            name='TitleStyle',
-            fontName='Courier',
-            fontSize=24,
-            leading=0,  # Line height
-            alignment=0,  # Center alignment
-            textColor=color
-        ))
+    # stylesheet object
+    custom_styles = getSampleStyleSheet()
 
-        custom_styles.add(ParagraphStyle(
-            name='BodyStyle',
-            fontName='Courier',
-            fontSize=12,
-            leading=0,
-            alignment=0,  # Left alignment
-            textColor=colors.black
-        ))
-    except:
-        pass
+    # modify the custom stylesheet
+    custom_styles.add(ParagraphStyle(
+        name='TitleStyle',
+        fontName='Courier',
+        fontSize=24,
+        leading=0,  # Line height
+        alignment=0,  # Center alignment
+        textColor=color
+    ))
 
-    # Access styles like this:
+    custom_styles.add(ParagraphStyle(
+        name='BodyStyle',
+        fontName='Courier',
+        fontSize=12,
+        leading=0,
+        alignment=0,  # Left alignment
+        textColor=colors.black
+    ))
+
+    # access styles with these variables
     title_style = custom_styles['TitleStyle']
     body_style = custom_styles['BodyStyle']
     highlight_style = custom_styles['BodyStyle']
 
+    # generate a doc name based on the warband name
     filename = f"{data['Name']}.pdf"
+    # make the doc with reportlab
     doc = SimpleDocTemplate(filename, pagesize=letter)
 
+    # store the letter page width and height to variables
     page_width, page_height = letter  # Default letter size (8.5 x 11 inches)
     
+    # the contents of the document
     content = []
+    # warband name as a title
     content.append(Paragraph(data["Name"], title_style))
 
+    # spacer
     content.append(Spacer(page_width, inch * 0.5))
 
+    # main table style
     table_style = TableStyle(
         [
             ('INNERGRID', (0,0), (-1,-1), 0.25, (0, 0, 0)),
@@ -114,38 +135,35 @@ def generate_pdf_with_table(data, ignore_tough, corner_rounding, page_splitting,
             ('ROUNDEDCORNERS', [8 * corner_rounding] * 4)
         ]
     )
-
-    table_style_3 = TableStyle(
-        [
-            ('INNERGRID', (0,0), (-1,-1), 0.25, (0, 0, 0)),
-            ('BOX', (0,0), (-1,-1), 0.25, (0, 0, 0)),
-            ('FONTNAME', (0, 0), (-1, -1), "Courier"),
-            ('BACKGROUND', (0, 0), (-1, -1), color)
-        ]
-    )
     
+    # secondary table style
     table_style_2 = TableStyle(
         [
             ('FONTNAME', (0, 0), (-1, -1), "Courier")
         ]
     )
 
-    # Data for the table
+    # data for the overarching table
     table_data = [
         [ "Faction: " + str(data["Faction"]["Name"]) ] , [Table([[ "Ducats: " + str(data["DucatCost"]), "Glory: " + str(data["GloryCost"]) ]], colWidths=None, style=table_style)]
     ]
 
-    # Create the table and apply a style
+    # create the table and apply a style
     table = Table(table_data, colWidths=None)
     table.setStyle(table_style)
 
+    # add the table to the content
     content.append(table)
+    # spacer
     content.append(Spacer(page_width, inch * 0.5))
 
     for member in data["Members"]:
         try:
+            # some widely-used dictionary objects
             model = member["Model"]
             obj = model["Object"]
+            
+            # add all traits from the model
             traits = "Traits: "
 
             for trait in obj["Tags"]:
@@ -153,16 +171,18 @@ def generate_pdf_with_table(data, ignore_tough, corner_rounding, page_splitting,
             
             traits = traits.removesuffix(', ')
         
-            # Define the outer table's data
+            # define the model's outer table data
             outer_data = [
                 [ member["Name"], traits ],
                 [Table([[f"Base: {obj['Base'][0]}", f"Range: {literal(obj['Ranged'][0])}", f"Melee: {literal(obj['Melee'][0])}"]], colWidths=None, style=table_style), Table([[f"Move: {obj['Movement'][0]}\"", obj['Name']]], colWidths=None, style=table_style)],
             ]
 
+            # define the model's weapon table data
             weapon_data = [
                 ['Weapons:'],
             ]
             
+            # populate the model's weapons
             for weapon in member["Equipment"]:
                 weapon_obj = weapon["Object"]
                 if weapon_obj["EquipType"]:
@@ -183,10 +203,12 @@ def generate_pdf_with_table(data, ignore_tough, corner_rounding, page_splitting,
                     if len(weapon_obj["Description"]):
                         weapon_data.append([Table([[f"Rules:\n{cursed_weapon(weapon_obj)}"]], colWidths=None, style=table_style_2)])
 
+            # define the model's abilities table data
             abilities_data = [
                 ['Abilities/Upgrades:'],
             ]
-            
+
+            # populate the abilities and upgrades            
             string = ""
 
             for ability in obj["Abilities"]:
@@ -198,6 +220,8 @@ def generate_pdf_with_table(data, ignore_tough, corner_rounding, page_splitting,
             for upgrade in member["Upgrades"]:
                 string += "\n• " + upgrade["Name"] + ":\n" + "\n".join(split(upgrade["Description"][0]["Content"]))
 
+            # split a string that's too long into two
+            # will crash otherwise
             if string != "":
                 if len(string.splitlines()) > 50:
                     line1 = "\n".join(string.splitlines()[:50])
@@ -207,10 +231,12 @@ def generate_pdf_with_table(data, ignore_tough, corner_rounding, page_splitting,
                 else:
                     abilities_data.append([Table([[string]], colWidths=None, style=table_style_2)])
 
+            # define the model's skills table data
             skills_data = [
                 ['Skills/Injuries:'],
             ]
-            
+
+            # populate the skills and injuries
             string = ""
 
             for skill in member["Skills"]:
@@ -219,6 +245,8 @@ def generate_pdf_with_table(data, ignore_tough, corner_rounding, page_splitting,
             for injury in member["Injuries"]:
                 string += "\n• " + injury["Name"] + ":\n" + "\n".join(split(injury["Description"][0]["Content"]))
 
+            # split a string that's too long into two
+            # will crash otherwise
             if string != "":
                 if len(string.splitlines()) > 50:
                     line1 = "\n".join(string.splitlines()[:50])
@@ -228,10 +256,12 @@ def generate_pdf_with_table(data, ignore_tough, corner_rounding, page_splitting,
                 else:
                     skills_data.append([Table([[string]], colWidths=None, style=table_style_2)])
             
+            # define the model's equipment table data
             equipment_data = [
                 ['Equipment:'],
             ]
-            
+
+            # populate the equipment
             string = ""
 
             for equipment in member["Equipment"]:
@@ -242,26 +272,36 @@ def generate_pdf_with_table(data, ignore_tough, corner_rounding, page_splitting,
             if string != "":
                 equipment_data.append([Table([[string]], colWidths=None, style=table_style_2)])
 
-            # Rebuild the outer table with the nested tables
+            # build the outer table
             outer_table = Table(outer_data, colWidths=None, rowHeights=0.35*inch)    
             outer_table.setStyle(table_style)
             content.append(outer_table)
+            
+            # build weapon table and add it to content if not empty
             if len(weapon_data) != 1:
                 weapon_table = Table(weapon_data, colWidths=None)
                 weapon_table.setStyle(table_style)
                 content.append(weapon_table)
+            
+            # build ability table and add it to content if not empty
             if len(abilities_data) != 1:
                 abilities_table = Table(abilities_data, colWidths=None)
                 abilities_table.setStyle(table_style)
                 content.append(abilities_table)
+            
+            # build skill table and add it to content if not empty
             if len(skills_data) != 1:
                 skills_table = Table(skills_data, colWidths=None)
                 skills_table.setStyle(table_style)
                 content.append(skills_table)
+            
+            # build equipment table and add it to content if not empty
             if len(equipment_data) != 1:
                 equipment_table = Table(equipment_data, colWidths=None)
                 equipment_table.setStyle(table_style)
                 content.append(equipment_table)
+            
+            # split models based on page_splitting flag
             if page_splitting:
                 content.append(PageBreak())
             else:
@@ -269,38 +309,52 @@ def generate_pdf_with_table(data, ignore_tough, corner_rounding, page_splitting,
         except IndexError:
             pass
 
+    # build the document
     doc.build(content)
 
+    # return the filename of the document so
+    # that flask can download it for the user
     return doc.filename
-    
-app = Flask(__name__)
 
+# make the app object and configure it
+app = Flask(__name__)
 app.config['ALLOWED_EXTENSIONS'] = {'json'}
 
-# Check allowed file extensions
+# check allowed file extensions
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
+# about page
 @app.route("/about")
 def about():
     return render_template("about.html")
 
+# home page
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
+        # filename
         f = request.files.get('file_input')
+
+        # flags
         ignore_tough = 'ignore_tough' in request.form.getlist('checkbox1')
         rounded_corners = 'rounded_corners' in request.form.getlist('checkbox2')
         page_splitting = 'page_splitting' in request.form.getlist('checkbox3')
+        
+        # warband color
         highlight_color = request.form.getlist("colorPicker")[0]
         
         if f and allowed_file(f.filename):
+            # load the json and generate the data
             data = json.load(f)
             name = generate_pdf_with_table(data, ignore_tough, rounded_corners, page_splitting, highlight_color)
     
+            # download the file on the user's machine
             return send_file(name, mimetype="application/pdf", as_attachment=True)
 
+    # render the home page html
     return render_template('index.html')
 
+# run the app
 if __name__ == "__main__":
     app.run(debug=True)
