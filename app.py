a98c01a3a8e65f776442821b623c95d1241c7a82
kululu00 <122434344+kululu00@users.noreply.github.com>
2025-03-21 11:02:52 -0500

    Update app.py
    Testing

--- app.py
+++ app.py
@@ -249,7 +249,8 @@ def generate_pdf(data, ignore_tough, corner_rounding, page_splitting, color):
                 string += "\n• " + a["name"] + ":\n" + cursed(a)
 
             for upgrade in member["Upgrades"]:
-                string += "\n• " + upgrade["Name"] + ":\n" + "\n".join(split(upgrade["Description"][0]["Content"]))
+                a = upgrade["Description"]
+                string += "\n• " + upgrade["Name"] + ":\n" + cursed(a)
 
             # split a string that's too long into two
             # will crash otherwise
